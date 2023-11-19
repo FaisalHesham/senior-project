29437d67f590d75a32aad1261258c8f5e75a3e90
Ali Alnujaidi <ali.alnujaidi4@gmail.com>
2023-11-19 17:37:35 +0300

    g

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,52 @@
-import Image from 'next/image'
-import styles from './page.module.css'
+"use client";
+import React, { useRef, useState } from "react";
+import WaveSurfer from "wavesurfer.js";
 
-export default function Home() {
+const AudioGraph = () => {
+  const [waveSurfer, setWaveSurfer] = useState(null);
+  const inputRef = useRef(null);
+
+  const handleFileChange = (event: any) => {
+    const file = event.target.files[0];
+
+    if (file) {
+      const reader = new FileReader();
+
+      reader.onload = (e: any) => {
+        const audioData = e.target.result;
+        console.log(new Blob([audioData]));
+        if (waveSurfer) {
+          waveSurfer.destroy();
+        }
+
+        const wavesurferInstance = WaveSurfer.create({
+          container: "#waveform",
+          waveColor: "violet",
+          progressColor: "purple",
+          height: 'auto',
+          width: 500,
+
+        });
+
+        wavesurferInstance.loadBlob(new Blob([audioData]));
+        setWaveSurfer(wavesurferInstance);
+      };
+
+      reader.readAsArrayBuffer(file);
+    }
+  };
+
   return (
-    <p>hh</p>
-  )
-}
+    <div>
+      <input
+        type="file"
+        accept=".wav"
+        onChange={handleFileChange}
+        ref={inputRef}
+      />
+      <div id="waveform"></div>
+    </div>
+  );
+};
+
+export default AudioGraph;
